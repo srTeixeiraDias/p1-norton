0adf584ab9073c79b78fcdfac844e5158506884e
Kayky Freitas <kayky_defreitas@hotmail.com>
2023-10-06 14:53:35 -0300

    ajustando links e rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 
 const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: "vitrine", component: VitrineComponent },
   { path: "cesta", component: CestaComponent },
   { path: "esqueci-senha", component: EsqueciSenhaComponent },
